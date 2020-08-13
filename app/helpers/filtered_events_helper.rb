3db3797d05f0c4030c832169d6c98e9491b96272
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-13 09:53:35 -0700

    adding filtered_events controller to use with vue

---
+++ app/helpers/filtered_events_helper.rb
@@ -0,0 +1,2 @@
+module FilteredEventsHelper
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  def self.between(start_date= Time::now.to_date, end_date = Time::now.to_date)
+    where('begins_at < ? AND ends_at > ? ', start_date, end_date)
+  end
 end
