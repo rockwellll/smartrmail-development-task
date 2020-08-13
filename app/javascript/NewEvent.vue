091126efdbf20c51c1eadd3728dc301867caf2a5
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-13 14:37:14 -0700

    formatting

--- app/javascript/NewEvent.vue
+++ app/javascript/NewEvent.vue
@@ -1,20 +1,26 @@
 <template>
-    <div>
-        <button class="p-2 rounded rounded-sm bg-blue-700 hover:bg-blue-500 text-white">
-            New Event!
-        </button>
-    </div>
+  <div>
+    <button
+      @click="showModal= !showModal"
+      class="p-2 rounded rounded-sm bg-blue-700 hover:bg-blue-500 text-white"
+    >New Event!</button>
+
+    <modal v-on:close="showModal= !showModal" :show-modal="showModal">
+      <h1>Create New Product</h1>
+
+      <slot></slot>
+    </modal>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "NewEvent",
-        data: () => ({
-            showModal: false
-        })
-    }
+export default {
+  name: "NewEvent",
+  data: () => ({
+    showModal: false,
+  }),
+};
 </script>
 
 <style scoped>
-
 </style>
