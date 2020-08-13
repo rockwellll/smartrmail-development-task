49a3478a4f0c7f946009a27ea518ca491d2d201f
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-13 13:34:30 -0700

    Adding the views for events and singleEvent, and removing unused code from index

---
+++ app/javascript/EventView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <div class="rounded rounded-sm bg-blue-600 text-white p-4 self-center">
+        <span class="text-xs">{{events[0].name}}</span>
+      </div>
+
+      <button v-if="events.length" class="text-sm" @click="showModal= !showModal">View More</button>
+    </div>
+
+    <div v-if="showModal" class="modal absolute top-0 left-0 w-full h-full">
+      <div class="flex w-full h-full justify-center items-center flex-col">
+        <div class="w-2/6 bg-white p-5 relative">
+          <button
+            class="focus:outline-none absolute right-0 top-0 mr-5 my-2"
+            @click="showModal= !showModal"
+          >X</button>
+          <single-event v-for="e in events" :event="e"></single-event>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventView",
+  props: ["events", "day", "month"],
+  data: () => ({
+    showModal: false,
+  }),
+};
+</script>
+
+<style scoped>
+.modal {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ app/javascript/SingleEvent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="rounded rounded-sm border border-b-2 border-gray-400 text-white p-4 self-center text-black my-2 text-left"
+  >
+    <span class="text-xs text-black">{{event.name}}</span>
+    <div class="flex flex-col text-black">
+      <span>Starts At: {{event.begins_at}}</span>
+      <span>Ends At: {{event.ends_at}}</span>
+
+      <p>{{event.description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleEvent",
+  props: ["event"],
+};
+</script>
