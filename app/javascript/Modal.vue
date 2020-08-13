0259aacdc435f58fa9fc0beef005e226ac27839a
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-13 13:50:06 -0700

    refactoring the modal code into separate component for re-usability

---
+++ app/javascript/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="showModal" class="modal absolute top-0 left-0 w-full h-full">
+        <div class="flex w-full h-full justify-center items-center flex-col ">
+            <div class="w-2/6 bg-white p-5 relative rounded rounded-lg">
+                <button
+                        class="focus:outline-none absolute right-0 top-0 mr-5 my-2"
+                        @click="$emit('close')"
+                >X</button>
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Modal",
+        data: ()=> ({
+            showModal: this.props.showModal
+        }),
+        props: ["showModal"]
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/javascript/NewEvent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button class="p-2 rounded rounded-sm bg-blue-700 hover:bg-blue-500 text-white">
+            New Event!
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewEvent",
+        data: () => ({
+            showModal: false
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
