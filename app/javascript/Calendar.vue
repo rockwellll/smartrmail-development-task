bd9149a8aa247ec382c09dcf79676cec88dde8c0
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-14 01:01:06 -0700

    adding the component, with access to store

---
+++ app/javascript/Calendar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import { mapMutations, mapState } from "vuex";
+    import events from "./store/events-store";
+    console.log()
+
+    export default {
+        name: "Calendar",
+        props: ['initial-events'],
+        computed: {
+            ...mapState(['events'])
+        },
+        mounted: function() {
+            this.setEvents(
+                this.$props.initialEvents
+            );
+        },
+        methods: {
+            ...mapMutations(['setEvents'])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
