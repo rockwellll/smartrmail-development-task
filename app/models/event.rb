898b01533d16b8f17e7592fa12019b6397ebeff6
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-14 05:28:57 -0700

    adding method to format the dates for an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ApplicationRecord
   def self.between(start_date= Time::now.to_date, end_date = Time::now.to_date)
     where('begins_at < ? AND ends_at > ? ', start_date, end_date)
   end
+
+  def with_formatted_dates
+    {
+      name: name,
+        description: description,
+        begins_at: "#{begins_at.month }/ #{begins_at.day}",
+        ends_at: "#{ends_at.month }/ #{ends_at.day}"
+    }
+  end
 end
