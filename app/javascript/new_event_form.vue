cc41a751eda292c61ee7c21ec4ba245e270005b8
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 03:10:36 -0700

    adding methods to reste the inputs when the form is submitted successfully to the server. added two extra properties to the added event calle

--- app/javascript/new_event_form.vue
+++ app/javascript/new_event_form.vue
@@ -2,7 +2,7 @@
   <div>
     <button
       @click="showModal= !showModal"
-      class="p-2 rounded rounded-sm bg-blue-700 hover:bg-blue-500 text-white"
+      class="p-2 rounded-lg bg-primary text-white"
     >New Event!</button>
 
     <modal v-on:close="showModal= !showModal" :show-modal="showModal">
@@ -18,9 +18,9 @@
             type="text"
             name="name"
             v-model="name"
-            class="p-2 rounded bg-gray-300 focus:outline-none border focus:bg-white focus:border-blue-600 resize-none"
+            class="p-2 rounded bg-gray-100 focus:outline-none border focus:bg-white focus:border-primary"
+            placeholder="event name"
           />
-
 
           <label for="begins_at">Starts at</label>
           <input
@@ -30,7 +30,8 @@
             name="begins_at"
             value
             v-model="begins_at"
-            class="p-2 rounded bg-gray-300 border focus:outline-none focus:bg-white focus:border-blue-600 resize-none"
+            class="p-2 rounded bg-gray-100 border focus:outline-none focus:bg-white focus:border-primary"
+            placeholder="Event start date"
           />
 
           <label for="ends_at">Ends at</label>
@@ -39,29 +40,30 @@
             type="date"
             name="ends_at"
             id="ends_at"
-            value=""
+            value
             v-model="ends_at"
             :min="begins_at"
-            class="p-2 rounded bg-gray-300 border focus:outline-none focus:bg-white focus:border-blue-600 resize-none"
+            class="p-2 rounded bg-gray-100 border focus:outline-none focus:bg-white focus:border-primary"
+            placeholder="Event end date"
           />
 
           <label for="description">Description</label>
-          <input
+          <textarea
             v-bind:class="{'border-red-500': this.description.length === 0 && didSubmit }"
             id="description"
-            type="text"
             name="description"
             v-model="description"
-            class="p-2 rounded bg-gray-300 focus:bg-white border focus:outline-none focus:border-blue-600 resize-none"
-          />
+            class="p-2 rounded bg-gray-100 focus:bg-white border focus:outline-none focus:border-primary resize-none"
+            placeholder="description"
+          ></textarea>
         </div>
       </form>
 
-      <div class="actions">
+      <div class="actions my-2">
         <button
-          @click="addN"
+          @click="add"
           type="submit"
-          class="p-2 rounded rounded-sm bg-blue-700 hover:bg-blue-500 text-white"
+          class="p-2 rounded-lg border bg-primary-outline text-black"
         >Create!</button>
       </div>
     </modal>
@@ -80,6 +82,15 @@ export default {
     ends_at: "",
     id: new Date().toLocaleString(),
     didSubmit: false,
+    weekdays: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
   }),
   watch: {
     allEvents(old, newVal) {
@@ -89,25 +100,7 @@ export default {
   },
   methods: {
     ...mapMutations(["addEvent"]),
-    add: function () {
-      this.addEvent({
-        name: "added new",
-        description: "adklknasldkasf",
-        begins_at: new Date(),
-        ends_at: new Date(),
-      });
-    },
-
-    addN() {
-      console.log(JSON.stringify({
-        event: {
-          name: this.name,
-          description: this.description,
-          begins_at: this.begins_at,
-          ends_at: this.ends_at,
-        },
-      }));
-
+    add() {
       this.didSubmit = true;
       if (this.notValidInputs()) return;
 
@@ -125,12 +118,19 @@ export default {
           },
         }),
       })
-        .then((res) => {
-          console.log("res is",res);
+        .then(() => {
+          this.addEvent({
+            name: this.name,
+            description: this.description,
+            begins_at: this.begins_at,
+            ends_at: this.ends_at,
+            wday: this.weekdays[new Date(this.begins_at).getDay()],
+            weekNumber: this.getWeekNumber(new Date(this.begins_at)),
+          });
 
-          return res.json();
-      })
-        .then((res) => console.log("json res", res))
+          this.hideModal();
+          this.resetInputs();
+        })
         .catch((err) => console.log("err is ", err));
     },
 
@@ -142,9 +142,36 @@ export default {
         this.ends_at.length === 0
       );
     },
+
+    resetInputs() {
+      this.name = "";
+      this.description = "";
+      this.begins_at = "";
+      this.ends_at = "";
+      this.didSubmit = false;
+    },
+
+    hideModal() {
+      this.showModal = false;
+    },
+
+    getWeekNumber(d) {
+      d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+      d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+      const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+      const weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+      return weekNo;
+    },
   },
 };
 </script>
 
 <style scoped>
+  ::placeholder {
+    color: #D7D6F1;
+  }
+
+  input[type="date"]::placeholder {
+    color: red;
+  }
 </style>
