55e72ccc46b92bfc12473d94c09ab787db61b938
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 03:06:32 -0700

    changing how the date is displayed for an event, using substring with name and description to prevent them from messing up the UI

--- app/javascript/event.vue
+++ app/javascript/event.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="my-2 self-center w-2/3">
-      <div class="rounded rounded-sm bg-blue-600 text-white p-4 self-center text-xs text-left text-white">
-        <span>{{event.name}}</span>
-        <div class="flex flex-col">
-          <span>Starts At: {{event.begins_at.split("T")[0]}}</span>
-          <span>Ends At: {{event.ends_at.split("T")[0]}}</span>
+    <div class="rounded-sm bg-blue-600 p-4 self-center text-xs text-left text-white">
+      <span>{{event.name.substring(0, 10)}}...</span>
+      <div class="flex flex-col">
 
-          <p>{{event.description}}</p>
-        </div>
+        <span>
+          from {{event.begins_at.split("T")[0]}} to {{event.ends_at.split("T")[0]}}
+        </span>
+
+        <p>{{event.description.substring(0, 10)}}...</p>
       </div>
+    </div>
   </div>
 </template>
 
