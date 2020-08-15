7dd8093e1a509c63d0dcf08b178c9a89e60a2697
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 06:43:40 -0700

    updating the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_110301) do
+ActiveRecord::Schema.define(version: 2020_08_15_124901) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_08_13_110301) do
     t.datetime "ends_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "users"
+    t.string "location"
   end
 
 end
