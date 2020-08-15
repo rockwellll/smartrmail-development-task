0ef4fa5c21e54d947e83566c1d566193c07f66cf
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 04:42:02 -0700

    removing the watch property from the component, replacing the custom modal with the modal from the package

--- app/javascript/new_event_form.vue
+++ app/javascript/new_event_form.vue
@@ -1,70 +1,75 @@
 <template>
   <div>
-    <button
-      @click="showModal= !showModal"
-      class="p-2 rounded-lg bg-primary text-white"
-    >New Event!</button>
+    <button @click="showModal" class="p-2 rounded-lg bg-primary text-white">New Event!</button>
 
-    <modal v-on:close="showModal= !showModal" :show-modal="showModal">
-      <h1>Create New Product</h1>
+    <modal
+      height="auto"
+      classes="rounded-lg border-primary p-2"
+      :clickToClose="false"
+      name="new_event_form_modal"
+    >
+      <div class="py-2 px-5 flex flex-col">
+        <close-icon-button v-on:click="hideModal" />
+        <h1>Create New Product</h1>
 
-      <form action>
-        <div class="flex flex-col">
-          <label for="name">Name</label>
+        <form action>
+          <div class="flex flex-col">
+            <label for="name">Name</label>
 
-          <input
-            v-bind:class="{'border-red-500': name.length === 0 && didSubmit }"
-            id="name"
-            type="text"
-            name="name"
-            v-model="name"
-            class="p-2 rounded bg-gray-100 focus:outline-none border focus:bg-white focus:border-primary"
-            placeholder="event name"
-          />
+            <input
+              v-bind:class="{'border-red-500': name.length === 0 && didSubmit }"
+              id="name"
+              type="text"
+              name="name"
+              v-model="name"
+              class="p-2 rounded bg-gray-100 focus:outline-none border focus:bg-white focus:border-primary"
+              placeholder="event name"
+            />
 
-          <label for="begins_at">Starts at</label>
-          <input
-            v-bind:class="{'border-red-500': this.begins_at.length === 0 && didSubmit }"
-            type="date"
-            id="begins_at"
-            name="begins_at"
-            value
-            v-model="begins_at"
-            class="p-2 rounded bg-gray-100 border focus:outline-none focus:bg-white focus:border-primary"
-            placeholder="Event start date"
-          />
+            <label for="begins_at">Starts at</label>
+            <input
+              v-bind:class="{'border-red-500': this.begins_at.length === 0 && didSubmit }"
+              type="date"
+              id="begins_at"
+              name="begins_at"
+              value
+              v-model="begins_at"
+              class="p-2 rounded bg-gray-100 border focus:outline-none focus:bg-white focus:border-primary"
+              placeholder="Event start date"
+            />
 
-          <label for="ends_at">Ends at</label>
-          <input
-            v-bind:class="{'border-red-500': this.ends_at.length === 0 && didSubmit }"
-            type="date"
-            name="ends_at"
-            id="ends_at"
-            value
-            v-model="ends_at"
-            :min="begins_at"
-            class="p-2 rounded bg-gray-100 border focus:outline-none focus:bg-white focus:border-primary"
-            placeholder="Event end date"
-          />
+            <label for="ends_at">Ends at</label>
+            <input
+              v-bind:class="{'border-red-500': this.ends_at.length === 0 && didSubmit }"
+              type="date"
+              name="ends_at"
+              id="ends_at"
+              value
+              v-model="ends_at"
+              :min="begins_at"
+              class="p-2 rounded bg-gray-100 border focus:outline-none focus:bg-white focus:border-primary"
+              placeholder="Event end date"
+            />
 
-          <label for="description">Description</label>
-          <textarea
-            v-bind:class="{'border-red-500': this.description.length === 0 && didSubmit }"
-            id="description"
-            name="description"
-            v-model="description"
-            class="p-2 rounded bg-gray-100 focus:bg-white border focus:outline-none focus:border-primary resize-none"
-            placeholder="description"
-          ></textarea>
+            <label for="description">Description</label>
+            <textarea
+              v-bind:class="{'border-red-500': this.description.length === 0 && didSubmit }"
+              id="description"
+              name="description"
+              v-model="description"
+              class="p-2 rounded bg-gray-100 focus:bg-white border focus:outline-none focus:border-primary resize-none"
+              placeholder="description"
+            ></textarea>
+          </div>
+        </form>
+
+        <div class="actions my-2">
+          <button
+            @click="add"
+            type="submit"
+            class="p-2 rounded-lg border bg-primary-outline text-black"
+          >Create</button>
         </div>
-      </form>
-
-      <div class="actions my-2">
-        <button
-          @click="add"
-          type="submit"
-          class="p-2 rounded-lg border bg-primary-outline text-black"
-        >Create!</button>
       </div>
     </modal>
   </div>
@@ -77,7 +82,6 @@ import axios from "axios";
 export default {
   name: "NewEvent",
   data: () => ({
-    showModal: false,
     name: "",
     description: "",
     begins_at: "",
@@ -94,24 +98,19 @@ export default {
       "Sunday",
     ],
   }),
-  watch: {
-    allEvents(old, newVal) {
-      console.log("old is", old);
-      console.log("new is", newVal);
-    },
-  },
   methods: {
     ...mapMutations(["addEvent"]),
     add() {
       this.didSubmit = true;
       if (this.notValidInputs()) return;
 
-      axios.post("/events.json", {
-        name: this.name,
-        description: this.description,
-        begins_at: this.begins_at,
-        ends_at: this.ends_at,
-      })
+      axios
+        .post("/events.json", {
+          name: this.name,
+          description: this.description,
+          begins_at: this.begins_at,
+          ends_at: this.ends_at,
+        })
         .then(() => {
           this.addEvent({
             name: this.name,
@@ -146,7 +145,7 @@ export default {
     },
 
     hideModal() {
-      this.showModal = false;
+      this.$modal.hide("new_event_form_modal");
     },
 
     getWeekNumber(d) {
@@ -156,12 +155,16 @@ export default {
       const weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
       return weekNo;
     },
+
+    showModal() {
+      this.$modal.show("new_event_form_modal");
+    },
   },
 };
 </script>
 
 <style scoped>
-  ::placeholder {
-    color: #D7D6F1;
-  }
+::placeholder {
+  color: #d7d6f1;
+}
 </style>
