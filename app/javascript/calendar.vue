fd9f21e53c30fa483f5674e6a774c19a1fa180b5
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 12:21:07 -0700

    adding the hedaing for the table, adding ordinalize method which ordinalizes numbers

--- app/javascript/calendar.vue
+++ app/javascript/calendar.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <slot></slot>
+  <div class="flex flex-col">
+    <h1
+      class="text-2xl self-center color-primary my-2 uppercase"
+    >There are {{allEvents.length}} events</h1>
 
     <table class="w-full text-center">
       <thead>
         <tr>
-          <th class="w-1/12" v-for="day in weekdays"></th>
+          <th
+            class="py-1 border w-1/12"
+            v-for="(day, index) in weekdays"
+            style="border-color: #D7D6F1"
+          >
+            <h1 class="color-primary">{{day}}</h1>
+            <div class="flex-1 flex flex-row color-tertiary justify-center font-light">
+              <h1>{{calculateDayNumberFor(firstDayOfWeek+index)}}</h1>
+              <h2 class="mx-2">{{month}}</h2>
+            </div>
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -30,7 +42,7 @@ import { mapMutations, mapGetters } from "vuex";
 
 export default {
   name: "Calendar",
-  props: ["initial-events", "week-number"],
+  props: ["initial-events", "week-number", "first-day-of-week", "month"],
   data: () => ({
     weekdays: [
       "Monday",
@@ -50,6 +62,20 @@ export default {
   },
   methods: {
     ...mapMutations(["setEvents"]),
+    calculateDayNumberFor(dayNumber) {
+      let suffix = "th";
+      const number = dayNumber.toString();
+
+      if (number.endsWith("1")) {
+        suffix = "st";
+      } else if (number.endsWith("2")) {
+        suffix = "nd";
+      } else if (number.endsWith("3")) {
+        suffix = "rd";
+      }
+
+      return `${number}${suffix}`;
+    },
   },
 };
 </script>
