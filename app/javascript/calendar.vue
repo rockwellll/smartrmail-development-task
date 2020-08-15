fd6c9d5bea0c378789e85a490270eadaba81f629
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 06:46:16 -0700

    passing the index to the event component

--- app/javascript/calendar.vue
+++ app/javascript/calendar.vue
@@ -9,14 +9,14 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="event in allEvents" class="w-full">
+        <tr v-for="(event, index) in allEvents" class="w-full">
           <td v-for="(day, i) in weekdays" :key="i">
             <div
               v-if="event.wday === day && event.weekNumber.toString() === weekNumber.toString()"
               class="flex justify-center items-center text-gray-600 flex-col my-3"
               style="height: 100px;"
             >
-              <event :key="event.id" :event="event"></event>
+              <event :key="index" :event="event" :index="index"></event>
             </div>
           </td>
         </tr>
