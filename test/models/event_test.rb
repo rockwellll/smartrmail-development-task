532898102096e5b6baf2ad4860617ee46cbe5343
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 12:53:41 -0700

    adding tests to check the validity and format of the event model

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,4 +4,15 @@ class EventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+
+  test 'it converts the event object to a hash, and formats the dates to strings, adding wday, day, and weekNumber to the event hash' do
+    event = events(:one)
+
+    hashed_event = event.with_formatted_dates
+    result_date = "#{event.begins_at.month}/#{event.begins_at.day}"
+
+    assert hashed_event[:begins_at] == result_date
+    assert !hashed_event[:day].nil?
+  end
 end
