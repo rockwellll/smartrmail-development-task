0241cce77195363c4f482d34ead5daaadb5155d2
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 04:41:01 -0700

    removing the custom modal and replacing it with modal from package, adding methods to close and open the modal.

--- app/javascript/event.vue
+++ app/javascript/event.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-2 self-center w-2/3">
-    <div class="rounded-sm bg-blue-600 p-4 self-center text-xs text-left text-white">
+    <div class="rounded-sm p-4 self-center text-xs text-left text-white border border-primary bg-primary">
       <span>{{event.name.substring(0, 10)}}...</span>
       <div class="flex flex-col">
 
@@ -11,16 +11,43 @@
         <p>{{event.description.substring(0, 10)}}...</p>
       </div>
 
-      <button>
+      <button class="focus:outline-none underline" @click="showModal">
         view more
       </button>
     </div>
+
+    <modal classes="p-5 relative" height="auto" :name="event.id.toString().concat('event_modal')">
+        <div class="flex flex-col">
+          <close-icon-button v-on:click="hideModal" class="absolute top-0"/>
+
+          <div class="text-left w-full">
+            <span>{{event.name}}</span>
+            <div class="flex flex-col">
+
+        <span>
+          from {{event.begins_at.split("T")[0]}} to {{event.ends_at.split("T")[0]}}
+        </span>
+
+              <p>{{event.description}}</p>
+            </div>
+          </div>
+        </div>
+    </modal>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "EventView",
   props: ["event"],
+  methods: {
+    showModal() {
+       this.$modal.show(this.$props.event.id.toString().concat('event_modal'));
+    },
+    hideModal() {
+      this.$modal.hide(this.$props.event.id.toString().concat('event_modal'));
+    }
+  }
 };
 </script>
