45ddad1ff807437e487b30391714494b1bc19aad
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-15 05:51:22 -0700

    changing the colors of the containers, and reformatting code

--- app/javascript/event.vue
+++ app/javascript/event.vue
@@ -1,53 +1,50 @@
 <template>
   <div class="my-2 self-center w-2/3">
-    <div class="rounded-sm p-4 self-center text-xs text-left text-white border border-primary bg-primary">
+    <div
+      class="rounded-lg p-4 self-center text-xs text-left text-white border border-primary bg-primary"
+    >
       <span>{{event.name.substring(0, 10)}}...</span>
       <div class="flex flex-col">
-
-        <span>
-          from {{event.begins_at.split("T")[0]}} to {{event.ends_at.split("T")[0]}}
-        </span>
+        <span>from {{event.begins_at.split("T")[0]}} to {{event.ends_at.split("T")[0]}}</span>
 
         <p>{{event.description.substring(0, 10)}}...</p>
       </div>
 
-      <button class="focus:outline-none underline" @click="showModal">
-        view more
-      </button>
+      <button class="focus:outline-none underline" @click="showModal">view more</button>
     </div>
 
-    <modal classes="p-5 relative" height="auto" :name="event.id.toString().concat('event_modal')">
-        <div class="flex flex-col">
-          <close-icon-button v-on:click="hideModal" class="absolute top-0"/>
+    <modal
+      style="min-height: 200px"
+      classes="p-5 relative rounded-lg"
+      :name="event.id.toString().concat('event_modal')"
+    >
+      <div class="flex flex-col h-full">
+        <close-icon-button v-on:click="hideModal" class="absolute top-0" />
 
-          <div class="text-left w-full">
-            <span>{{event.name}}</span>
-            <div class="flex flex-col">
+        <div class="text-left w-full h-full flex flex-col justify-center">
+          <span class="uppercase text-bold color-primary text-lg">{{event.name}}</span>
+          <div class="flex flex-col mt-4">
+            <span>from {{event.begins_at.split("T")[0]}} to {{event.ends_at.split("T")[0]}}</span>
 
-        <span>
-          from {{event.begins_at.split("T")[0]}} to {{event.ends_at.split("T")[0]}}
-        </span>
-
-              <p>{{event.description}}</p>
-            </div>
+            <p class="mt-2">{{event.description}}</p>
           </div>
         </div>
+      </div>
     </modal>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "EventView",
   props: ["event"],
   methods: {
     showModal() {
-       this.$modal.show(this.$props.event.id.toString().concat('event_modal'));
+      this.$modal.show(this.$props.event.id.toString().concat("event_modal"));
     },
     hideModal() {
-      this.$modal.hide(this.$props.event.id.toString().concat('event_modal'));
-    }
-  }
+      this.$modal.hide(this.$props.event.id.toString().concat("event_modal"));
+    },
+  },
 };
 </script>
