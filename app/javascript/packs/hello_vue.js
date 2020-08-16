106fd8af47b6e673a738f0668154049c7f9f4e30
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-16 04:05:30 -0700

    importing and regestering the TableHeader component

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,6 +7,7 @@ import Event  from "../event";
 import NewEvent from "../new_event_form";
 import Calendar from "../calendar";
 import CloseIconButton from "../close_icon_button";
+import TableHeader from "../table_header";
 
 import events from "../store/events_store";
 import { createConsumerFor } from "../channels/events_channel";
@@ -16,10 +17,12 @@ Vue.use(TurbolinksAdapter)
 Vue.use(Vuex);
 Vue.use(VModal);
 
+
 Vue.component('event', Event);
 Vue.component('new-event-form', NewEvent);
 Vue.component('calendar', Calendar);
 Vue.component('close-icon-button', CloseIconButton);
+Vue.component('table-header', TableHeader);
 
 const mainStore = new Vuex.Store({
   modules: {
