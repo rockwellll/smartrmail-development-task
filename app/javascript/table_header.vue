f8bcdf9f45dbfe673a8042f4d8282ff8909e7740
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-16 04:01:36 -0700

    refactoring the header for a date into this component for reusability

---
+++ app/javascript/table_header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <h1 class="color-primary">{{day}}</h1>
+      <div class="flex-1 flex flex-row color-tertiary justify-center font-light">
+          <h1>{{ordinalize(firstDayOfWeek+index)}}</h1>
+          <h2 class="mx-2">{{month}}</h2>
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ["day", "firstDayOfWeek", "index", "month"],
+        methods: {
+            ordinalize(dayNumber) {
+                let suffix = "th";
+                const number = dayNumber.toString();
+
+                if (number.endsWith("1")) {
+                    suffix = "st";
+                } else if (number.endsWith("2")) {
+                    suffix = "nd";
+                } else if (number.endsWith("3")) {
+                    suffix = "rd";
+                }
+
+                return `${number}${suffix}`;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
