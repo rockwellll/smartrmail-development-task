1a1d7b7ad2f75874c5e6d905f979c58ad675151f
A7madXatab <Ahmadzendane725@gmail.com>
2020-08-16 02:04:26 -0700

    mounting the action cable server, and removing the route registration for the filtered_events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :events
-
-  get '/filtered-events', to: 'filtered_events#index'
   get '/', to: 'events#index'
+
+  mount ActionCable.server => "/events"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
